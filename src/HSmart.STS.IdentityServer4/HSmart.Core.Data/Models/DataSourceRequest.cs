56c1f0ea074eb42535ee478300c0d55b5a2ddcab
hoangnc2 <hoangnc2@fsoft.com.vn>
2019-09-24 15:22:00 +0700

    Init Source
    - Init Source

---
+++ src/HSmart.STS.IdentityServer4/HSmart.Core.Data/Models/DataSourceRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace DT.Core.Data.Models
+{
+    public class DataSourceRequest
+    {
+        public int FiltersCount { get; set; }
+        public string SortDataField { get; set; }
+        public string SortOrder { get; set; }
+        public int GroupsCount { get; set; }
+        public int PageNum { get; set; }
+        public int PageSize { get; set; }
+        public int RecordStartIndex { get; set; }
+        public int RecordEndIndex { get; set; }
+    }
+}
